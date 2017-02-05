e4e721cda00055ca80c93be4650919e4c409a2fc
jdong28 <xiuxuandong@hotmail.com>
2017-02-04 21:00:13 -0500

    For tyson

--- src/fydp/model/CarCharger.java
+++ src/fydp/model/CarCharger.java
@@ -1,11 +1,21 @@
 /**
  * Stores information about a single charger and the attached car.
  */
+
+/**
+ * Calculate Priority
+ * Assign 1 and 2 and 0s
+ */
 package fydp.model;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CarCharger {
+    //priority of charging
+    private double chargePriority;
+
+    //hours to fully charge vehicle
+    private double fullChargeTime = 4;
 
     //Start time, half hour intervals
     private double[] chargeTime = new double[48];
@@ -18,6 +28,22 @@ public class CarCharger {
 
     //Rate of charge in kWH
     private double charge_rate;
+
+    public double getChargePriority() {
+        return chargePriority;
+    }
+
+    public void setChargePriority(double chargePriority) {
+        this.chargePriority = chargePriority;
+    }
+
+    public double getFullChargeTime() {
+        return fullChargeTime;
+    }
+
+    public void setFullChargeTime(double fullChargeTime) {
+        this.fullChargeTime = fullChargeTime;
+    }
 
     public double[] getChargeTime() {
         return chargeTime;
@@ -59,6 +85,7 @@ public class CarCharger {
     }
 
     public CarCharger() {
+        //assume each car takes 4 hours to charge
 
         //assume charge from 8AM to 12PM
         for (int i = 16; i < 24; i++) {
@@ -71,5 +98,20 @@ public class CarCharger {
         charge_rate = 3.3;
 
     }
+
+    double CalculateChargeTime () {
+        int chargeSlotNumer = (int) Math.ceil(battery_level * fullChargeTime);
+
+        int startTime = ThreadLocalRandom.current().nextInt(7, 10);
+        int endTime = ThreadLocalRandom.current().nextInt(4,7);
+
+        for (int i = 0; i < 48; i ++) {
+        }
+
+    }
+
+
+
+
 
 }
